add4730e059f580167a283391399ea9ae8f28c83
navin <navinexpo2020>
2021-06-21 11:00:26 +0530

    learning dated 20th June 2021

--- JS/Tut10_function&scope.js
+++ JS/Tut10_function&scope.js
@@ -3,3 +3,22 @@ Function and Scope in Javascript
 ------*/ 
 
 console.log('This is a tut 10');
+
+function greet(name, thank){
+    console.log(`Happy bday ${name} 
+    how I wish I could be there with you 
+    to celebrate your bday and bombing
+    toegather. !! ${thank}`);
+}
+let name = 'skilln';
+let name2 = 'Navin';
+
+greet(name, 'Thanks a lot');
+
+const myobj = {
+    name: "skilln",
+    game: function(){
+        return "GTA"; 
+    }
+}
+console.log(myobj.game());
